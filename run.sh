813077fb3e0817b2f5bb402ac0e20fd768301b1f
Ubuntu <ubuntu@ip-10-0-0-243.ap-southeast-2.compute.internal>
2018-01-23 02:44:05 +0000

    For unbuntu to sudo apt-get commands

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ DC_TAG=datacube-1.5.4
 
 # Upgrade and install dependencies
 # Postgres is installed and configured in order to run integration tests
-sudo yum update 
-sudo yum upgrade -y 
-sudo yum install -y wget bzip2 git postgresql gcc g++ ca-certificates 
-sudo yum clean
+sudo apt-get update 
+sudo apt-get upgrade -y 
+sudo apt-get install -y wget bzip2 git postgresql gcc g++ ca-certificates 
+sudo apt-get clean
 locale-gen en_US.UTF-8 en_US
 
 # Run datacube script
